06010cb7e10da5813a086315bb6a175b993b5552
sindhubedare <sindhubedare1990@gmail.com>
2020-08-13 18:54:51 -0500

    pseudocode

---
+++ src/pseudocode.js
@@ -0,0 +1,8 @@
+// draw wireframe and attach the image
+// 4 parts - Left side nav bar, header, content and footer
+//nav bar try to put the dog image
+// header
+//content - form with inputs
+// add toggle to male/female and spayed or Neutered
+//footer with 2 buttons - back and next
+// css styling to match the image given
